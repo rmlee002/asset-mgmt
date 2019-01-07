4ae8f39df3eac4532eefbedcd6aafbbd6c6dbf1b
Ryan Lee <rmlee002@gmail.com>
2019-01-07 16:40:09 -0500

    Adding functional navbar

---
+++ app/src/components/Software.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Links from './Nav';
+
+export default class Employees extends Component {
+    render(){
+        return(
+            <div>
+                <Links />
+                <h1>Software</h1>
+            </div>
+        );
+    }
+}
