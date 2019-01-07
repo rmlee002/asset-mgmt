9fb113afd1bb8c18028083f44c018d34942c60e5
Ryan Lee <rmlee002@gmail.com>
2019-01-07 12:15:24 -0500

    Organizing styles

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './Styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
