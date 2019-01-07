1c1bb2b1a4f5b9e4c82b8bc2a0fe55f738a80a53
Ryan Lee <rmlee002@gmail.com>
2019-01-07 15:07:33 -0500

    Organizing frontend routes

---
+++ app/src/components/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Employees from './Employees';
+import Hardware from './Hardware';
+import Software from './Software';
+import protect from './protect';
+
+export default () => 
+    <Switch>
+        <Redirect exact from='/' to='/home'/>
+        <Route path='/home' component={protect(Home)} />
+        <Route path='/login' component={Login} />
+        <Route path='/employees' component={protect(Employees)} />
+        <Route path='/hardware' component={protect(Hardware)} />
+        <Route path='/software' component={protect(Software)} />
+    </Switch>
