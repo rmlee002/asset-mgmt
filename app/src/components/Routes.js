b890b7642ca18131de550d700cffeb07546d05ed
Ryan Lee <rmlee002@gmail.com>
2019-01-16 13:04:00 -0500

    Removed ChangeOwner file

--- app/src/components/Routes.js
+++ app/src/components/Routes.js
@@ -9,7 +9,6 @@ import ManageEmployee from './Employees/Manage';
 import Assets from './Assets/Assets';
 import History from './Assets/History';
 import ManageAsset from './Assets/Manage';
-import ChangeOwner from './Assets/ChangeOwner';
 
 export default () => 
     <Switch>
@@ -20,7 +19,7 @@ export default () =>
         <Route exact path='/assets' component={protect(Assets)} />
         <Route path='/assets/history/:asset_id' component={protect(History)} />
         <Route exact path='/assets/manage/:asset_id' component={protect(ManageAsset)} />
-        <Route exact path='/assets/manage/:asset_id/editOwner' component={protect(ChangeOwner)} />
+        {/* <Route exact path='/assets/manage/:asset_id/editOwner' component={protect(ChangeOwner)} /> */}
         <Route path='/software' component={protect(Software)} />
         <Route path='/employees/manage/:emp_id' component={protect(ManageEmployee)} />
     </Switch>
