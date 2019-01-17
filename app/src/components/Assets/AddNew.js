15e3ade9d7213534e46826bbcc86c522f8b9d7e1
Ryan Lee <rmlee002@gmail.com>
2019-01-17 16:58:07 -0500

    Dynamic form component

--- app/src/components/Assets/AddNew.js
+++ app/src/components/Assets/AddNew.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Modal, Form, FormGroup, FormControl, ControlLabel, Col, HelpBlock} from 'react-bootstrap';
+import { Button, Modal, Form, FormGroup, FormControl, ControlLabel, Col, HelpBlock, ButtonToolbar } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import Links from '../Nav';
@@ -12,27 +12,43 @@ export default class AddAssets extends Component{
         this.handleChange = this.handleChange.bind(this);
         this.handleIn = this.handleIn.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.addAsset = this.addAsset.bind(this);
 
         this.state = {
             show: false,
-            description: null,
-            model: null,
-            serial_number: null,
             warranty_provider: null,
-            cost: null,
-            comment: null,
             vendor: null,
             order_num: null,
             warranty: null,
             inDate: null,
-            department: null
+            assets: [{
+                description: null,
+                model: null,
+                serial_number: null,               
+                cost: null,
+                comment: null,               
+                department: null
+            }]           
         }
     }
 
     handleChange(e){
-        this.setState({
-            [e.target.id]: nullify(e.target.value)
-        })
+        if (['description','model','serial_number','cost','comment','department'].includes(e.target.id)){
+            let assets = [...this.state.assets]
+            assets[e.target.dataset.id][e.target.id] = nullify(e.target.value)
+            this.setState({ assets })
+        }
+        else{
+            this.setState({
+                [e.target.id]: nullify(e.target.value)
+            }) 
+        }        
+    }
+
+    addAsset(e){
+        this.setState((prevState) => ({
+            assets: [...prevState.assets, {description:null, model:null, serial_number:null, cost:null, comment:null, department:null}]
+        }))
     }
 
     handleIn(date){
@@ -84,81 +100,15 @@ export default class AddAssets extends Component{
                 <Links />
                 <form onSubmit={this.handleSubmit}>
                     <Form horizontal>
-                        <FormGroup controlId='description'>
+                        <FormGroup controlId='order_num'>
                             <Col componentClass={ControlLabel} sm={3}>
-                                Description
+                                Order Number
                             </Col>
                             <Col sm={7}>
                                 <FormControl
                                     type='text'
-                                    value={this.state.description}
-                                    placeholder='Description'
-                                    onChange={this.handleChange}
-                                />
-                                <HelpBlock bsClass='small'>Required</HelpBlock>
-                            </Col>
-                        </FormGroup>
-                        <FormGroup controlId='model'>
-                            <Col componentClass={ControlLabel} sm={3}>
-                                Model
-                            </Col>
-                            <Col sm={7}>
-                                <FormControl
-                                    type='text'
-                                    value={this.state.model}
-                                    placeholder='Model'
-                                    onChange={this.handleChange}
-                                />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup controlId='serial_number'>
-                            <Col componentClass={ControlLabel} sm={3}>
-                                Serial Number
-                            </Col>
-                            <Col sm={7}>
-                                <FormControl
-                                    type='text'
-                                    value={this.state.serial_number}
-                                    placeholder='Serial Number'
-                                    onChange={this.handleChange}
-                                />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup controlId='warranty_provider'>
-                            <Col componentClass={ControlLabel} sm={3}>
-                                Warranty Provider
-                            </Col>
-                            <Col sm={7}>
-                                <FormControl
-                                    type='text'
-                                    value={this.state.warranty_provider}
-                                    placeholder='Warranty Provider'
-                                    onChange={this.handleChange}
-                                />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup controlId='cost'>
-                            <Col componentClass={ControlLabel} sm={3}>
-                                Cost
-                            </Col>
-                            <Col sm={7}>
-                                <FormControl
-                                    type='number'
-                                    value={this.state.cost}
-                                    placeholder='Cost'
-                                    onChange={this.handleChange}
-                                />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup controlId='comment'>
-                            <Col componentClass={ControlLabel} sm={3}>
-                                Comment
-                            </Col>
-                            <Col sm={7}>
-                                <FormControl
-                                    type='text'
-                                    value={this.state.comment}
-                                    placeholder='Comment'
+                                    value={this.state.order_num}
+                                    placeholder='Order Number'
                                     onChange={this.handleChange}
                                 />
                             </Col>
@@ -175,16 +125,27 @@ export default class AddAssets extends Component{
                                     onChange={this.handleChange}
                                 />
                             </Col>
-                        </FormGroup>
-                        <FormGroup controlId='order_num'>
+                        </FormGroup>         
+                        <FormGroup controlId='inDate'>
                             <Col componentClass={ControlLabel} sm={3}>
-                                Order Number
+                                In Date
+                            </Col>
+                            <Col sm={7}>
+                                <DatePicker
+                                    selected={this.state.inDate}
+                                    onChange={this.handleIn}
+                                />
+                            </Col>
+                        </FormGroup>               
+                        <FormGroup controlId='warranty_provider'>
+                            <Col componentClass={ControlLabel} sm={3}>
+                                Warranty Provider
                             </Col>
                             <Col sm={7}>
                                 <FormControl
                                     type='text'
-                                    value={this.state.order_num}
-                                    placeholder='Order Number'
+                                    value={this.state.warranty_provider}
+                                    placeholder='Warranty Provider'
                                     onChange={this.handleChange}
                                 />
                             </Col>
@@ -201,48 +162,117 @@ export default class AddAssets extends Component{
                                     onChange={this.handleChange}
                                 />
                             </Col>
-                        </FormGroup>
-                        <FormGroup controlId='inDate'>
-                            <Col componentClass={ControlLabel} sm={3}>
-                                In Date
-                            </Col>
-                            <Col sm={7}>
-                                <DatePicker
-                                    selected={this.state.inDate}
-                                    onChange={this.handleIn}
-                                />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup controlId='department'>
-                            <Col componentClass={ControlLabel} sm={3}>Department</Col>
-                            <Col sm={7}>
-                                <FormControl componentClass='select' onChange={this.handleChange}>
-                                    <option>Select...</option>
-                                    <option value='CQA'>CQA</option>
-                                    <option value='VPT'>VPT</option>
-                                    <option value='TCoE'>TCoE</option>
-                                    <option value='TI'>TI</option>
-                                    <option value='Sharepoint'>Sharepoint</option>                                        
-                                    <option value='NGC'>NGC</option>
-                                    <option value='NAVSUP'>NAVSUP</option>                                        
-                                    <option value='MHV'>MHV</option>
-                                    <option value='IDC'>IDC</option>                                        
-                                    <option value='VA'>VA</option>
-                                    <option value='BD'>BD</option>
-                                    <option value='Cm/Tools'>CM/Tools</option>
-                                    <option value='DITD'>DITD</option>
-                                    <option value='BD'>BD</option>
-                                    <option value='Northrop'>Northrop</option>
-                                    <option value='Overhead'>Overhead</option>
-                                    <option value='HR'>HR</option>
-                                    <option value='Accounting'>Accounting</option>
-                                    <option value='Legal'>Legal</option>
-                                    <option value='IT'>IT</option>
-                                </FormControl>
-                            </Col>
-                        </FormGroup>
+                        </FormGroup>                       
+                        {this.state.assets.map((item, index) => 
+                            <div>
+                                <FormGroup controlId='description'>
+                                    <Col componentClass={ControlLabel} sm={3}>
+                                        Description
+                                    </Col>
+                                    <Col sm={7}>
+                                        <FormControl
+                                            type='text'
+                                            value={this.state.description}
+                                            placeholder='Description'
+                                            onChange={this.handleChange}
+                                        />
+                                        <HelpBlock bsClass='small'>Required</HelpBlock>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup controlId='model'>
+                                    <Col componentClass={ControlLabel} sm={3}>
+                                        Model
+                                    </Col>
+                                    <Col sm={7}>
+                                        <FormControl
+                                            type='text'
+                                            value={this.state.model}
+                                            placeholder='Model'
+                                            onChange={this.handleChange}
+                                        />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup controlId='serial_number'>
+                                    <Col componentClass={ControlLabel} sm={3}>
+                                        Serial Number
+                                    </Col>
+                                    <Col sm={7}>
+                                        <FormControl
+                                            type='text'
+                                            value={this.state.serial_number}
+                                            placeholder='Serial Number'
+                                            onChange={this.handleChange}
+                                        />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup controlId='cost'>
+                                    <Col componentClass={ControlLabel} sm={3}>
+                                        Cost
+                                    </Col>
+                                    <Col sm={7}>
+                                        <FormControl
+                                            type='number'
+                                            value={this.state.cost}
+                                            placeholder='Cost'
+                                            onChange={this.handleChange}
+                                        />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup controlId='comment'>
+                                    <Col componentClass={ControlLabel} sm={3}>
+                                        Comment
+                                    </Col>
+                                    <Col sm={7}>
+                                        <FormControl
+                                            type='text'
+                                            value={this.state.comment}
+                                            placeholder='Comment'
+                                            onChange={this.handleChange}
+                                        />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup controlId='department'>
+                                    <Col componentClass={ControlLabel} sm={3}>Department</Col>
+                                    <Col sm={7}>
+                                        <FormControl componentClass='select' onChange={this.handleChange}>
+                                            <option>Select...</option>
+                                            <option value='CQA'>CQA</option>
+                                            <option value='VPT'>VPT</option>
+                                            <option value='TCoE'>TCoE</option>
+                                            <option value='TI'>TI</option>
+                                            <option value='Sharepoint'>Sharepoint</option>                                        
+                                            <option value='NGC'>NGC</option>
+                                            <option value='NAVSUP'>NAVSUP</option>                                        
+                                            <option value='MHV'>MHV</option>
+                                            <option value='IDC'>IDC</option>                                        
+                                            <option value='VA'>VA</option>
+                                            <option value='BD'>BD</option>
+                                            <option value='Cm/Tools'>CM/Tools</option>
+                                            <option value='DITD'>DITD</option>
+                                            <option value='BD'>BD</option>
+                                            <option value='Northrop'>Northrop</option>
+                                            <option value='Overhead'>Overhead</option>
+                                            <option value='HR'>HR</option>
+                                            <option value='Accounting'>Accounting</option>
+                                            <option value='Legal'>Legal</option>
+                                            <option value='IT'>IT</option>
+                                        </FormControl>
+                                    </Col>
+                                </FormGroup>
+                            </div>
+                        )}
+
+
+
+                        
                     </Form>
-                    <Button type='submit' bsStyle='success' disabled={!isValid}>Add Asset</Button>
+                    <ButtonToolbar>
+                        <Button onClick={this.addAsset} block>Add asset</Button>
+                    </ButtonToolbar>
+                    <ButtonToolbar>
+                        <Button type='submit' bsStyle='success' disabled={!isValid} block>Submit</Button>
+                    </ButtonToolbar>
+                    
                 </form>
             </div>
         );
