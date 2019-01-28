0c2bccdb985c11def7710f9a31985ea613285fef
Ryan Lee <rmlee002@gmail.com>
2019-01-28 14:37:34 -0500

    Protecting routes

--- app/src/components/Routes.js
+++ app/src/components/Routes.js
@@ -29,18 +29,18 @@ export default () =>
         <Route exact path='/employees' component={Employees} />
         <Route path='/employees/add' component={protect(AddEmployee)} />
         <Route path='/employees/manage/:emp_id' component={protect(ManageEmployee)} />
-        <Route exact path='/employees/:emp_id/assets' component={EmployeeAssets} />
+        <Route exact path='/employees/:emp_id/assets' component={protect(EmployeeAssets)} />
         <Route path ='/employees/:emp_id/addAsset' component={protect(AddAsset)} />
-        <Route exact path ='/employees/:emp_id/licenses' component={EmployeeLicenses} />
-        <Route path='/employees/:emp_id/licenses/add' component={AddEmployeeLicense} />
+        <Route exact path ='/employees/:emp_id/licenses' component={protect(EmployeeLicenses)} />
+        <Route path='/employees/:emp_id/licenses/add' component={protect(AddEmployeeLicense)} />
         <Route exact path='/assets' component={Assets} />
         <Route path='/assets/add' component={protect(AddAssets)} />
-        <Route path='/assets/:asset_id/history' component={History} />
+        <Route path='/assets/:asset_id/history' component={protect(History)} />
         <Route exact path='/assets/manage/:asset_id' component={protect(ManageAsset)} />
         <Route exact path='/assets/editOwner/:asset_id' component={protect(EditOwner)} />
         <Route exact path='/software' component={Software} />
-        <Route path='/software/:software_id/manage' component={ManageSoftware} />
+        <Route path='/software/:software_id/manage' component={protect(ManageSoftware)} />
         <Route path='/software/add' component={protect(AddSoftware)} />
         <Route exact path='/software/:software_id/users' component={Users} />
-        <Route path='/software/:software_id/users/add' component={AddUser} />
+        <Route path='/software/:software_id/users/add' component={protect(AddUser)} />
     </Switch>
