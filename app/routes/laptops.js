f64ab816932a19b61471bfc8600b81eff9567895
Ryan Lee <rmlee002@gmail.com>
2019-04-05 14:48:37 -0400

    Formatting

--- app/routes/laptops.js
+++ app/routes/laptops.js
@@ -3,8 +3,6 @@ const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
 const connection = require('../databases/assetDB.js');
-const request = require('request');
-const moment = require('moment')
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
