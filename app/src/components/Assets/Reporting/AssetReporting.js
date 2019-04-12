750f87c45729709a8bacfebf1b48a602fb2e9d53
Ryan Lee <rmlee002@gmail.com>
2019-04-12 14:56:30 -0400

    Reformatting reporting page

--- app/src/components/Assets/Reporting/AssetReporting.js
+++ app/src/components/Assets/Reporting/AssetReporting.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import { Tabs, Tab, Row, Col, Nav, NavItem } from 'react-bootstrap';
-import Axios from 'axios';
-import Data from './Data';
-import OldestDevices from './OldestDevices';
-import OOWDevices from './OOW';
-import BrokenDevices from './Broken';
+import React, { Component } from 'react'
+import { Nav, NavItem, Row, Col, Tabs, Tab } from 'react-bootstrap'
+// import { Switch, Route } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
+import Axios from 'axios'
+import Data from './Data'
+import OldestDevices from './OldestDevices'
+import OOWDevices from './OOW'
+import BrokenDevices from './Broken'
 
-export default class Reporting extends Component{
+export default class AssetReporting extends Component{
     constructor(props){
         super(props)
-
-        this.state = {
-            key: 'all',
-            departments: [],
+        this.state={
+            depts: [],
             data: [],
-            all: []
+            filtered: []
         }
 
         this.handleSelect = this.handleSelect.bind(this)
@@ -24,7 +24,7 @@ export default class Reporting extends Component{
         Axios.get('/departments')
         .then(res => {
             this.setState({
-                departments: res.data
+                depts: res.data
             })
         })
         .catch(err => {
@@ -34,79 +34,69 @@ export default class Reporting extends Component{
 
         Axios.get('/hwReporting')
         .then(res => {
-            this.setState({
-                data: res.data,
-                all: res.data
-            })
-        })
-        .catch(err =>{
-            if (err){
-                console.log(err)
-                alert(err.response.data)
+            if (this.props.match.params.contract === 'all'){
+                this.setState({
+                    data: res.data,
+                    filtered: res.data
+                })
+            }
+            else{
+                this.setState({
+                    data: res.data,
+                    filtered: res.data.filter(item => item.contract?item.contract.toLowerCase().split(',').includes(this.props.match.params.contract):false)
+                })
             }
         })
-
-        this.setState({
-            key: this.props.match.params.contract
+        .catch(err => {
+            console.log(err)
+            alert(err.response.data)
         })
     }
 
     handleSelect(key){
         if (key === 'all'){
             this.setState({
-                key: key,
-                data: this.state.all
+                filtered: this.state.data
             })
         }
         else{
             this.setState({
-                key: key,
-                data: this.state.all.filter(item => item.contract?item.contract.toLowerCase().split(',').includes(key):false)
+                filtered: this.state.data.filter(item => item.contract?item.contract.split(',').includes(key):false)
             })
-        }        
+        }
     }
 
     render(){
         return(
-            <React.Fragment>
-                <Tabs activeKey={this.state.key} onSelect={this.handleSelect}>
-                    <Tab eventKey="all" title="All"></Tab>
-                    {
-                        this.state.departments.map(option =>
-                            <Tab eventKey={option.label.toLowerCase()} title={option.label}></Tab>
-                        )
-                    }
-                </Tabs>
-
-                <Tab.Container defaultActiveKey='1'>
-                    <Row className="clearfix">
-                        <Col sm={2}>
-                            <Nav bsStyle="pills" stacked>
-                                <NavItem eventKey="1">Data</NavItem>
-                                <NavItem eventKey="2">Broken Devices</NavItem>
-                                <NavItem eventKey="3">Out of Warranty Devices</NavItem>
-                                <NavItem eventKey="4">Oldest Devices</NavItem>
-                            </Nav>
-                        </Col>
-                        <Col sm={10}>
-                            <Tab.Content animation>
-                                <Tab.Pane eventKey="1">
-                                    <Data/>
-                                </Tab.Pane>
-                                <Tab.Pane eventKey="2">
-                                    <BrokenDevices data={this.state.data} />    
-                                </Tab.Pane>
-                                <Tab.Pane eventKey="3">
-                                    <OOWDevices data={this.state.data} />
-                                </Tab.Pane>
-                                <Tab.Pane eventKey="4">
-                                    <OldestDevices data={this.state.data} />
-                                </Tab.Pane>
-                            </Tab.Content>
-                        </Col>
-                    </Row>
-                </Tab.Container>
-            </React.Fragment>
-        )
+            <Row className='clearfix'>
+                <Col sm={2}>
+                    <Nav bsStyle='pills' stacked activeKey={this.props.match.params.contract} onSelect={this.handleSelect}>
+                        <LinkContainer to='/assets/reporting/all'>
+                            <NavItem eventKey="all">All</NavItem>
+                        </LinkContainer>
+                        {this.state.depts.map(dept => 
+                            <LinkContainer to={`/assets/reporting/${dept.value}`}>
+                                <NavItem eventKey={dept.value}>{dept.label}</NavItem>
+                            </LinkContainer>)}
+                    </Nav>
+                </Col>
+                <Col sm={10}>
+                    <Tabs>
+                        <Tab eventKey="data" title="Data">
+                            <Data />
+                        </Tab>
+                        <Tab eventKey="broken" title="Broken Devices">
+                            <BrokenDevices data={this.state.filtered.filter(item => item.broken)}/>
+                        </Tab>
+                        <Tab eventKey="oow" title="Out of Warranty Devices">
+                            <OOWDevices data={this.state.filtered}/>
+                        </Tab>
+                        <Tab eventKey="oldest" title="Oldest Devices">
+                            <OldestDevices data={this.state.filtered}/>
+                        </Tab>
+                    </Tabs>
+                </Col>
+            </Row>
+        );
     }
 }
