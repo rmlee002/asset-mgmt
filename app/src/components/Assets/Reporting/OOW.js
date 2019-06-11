3db279c4b39d40bd8ca557d37b38357e045d188d
Ryan Lee <ryan.lee@vidoori.com>
2019-06-11 16:38:48 -0400

    Migrating to ReactTable

--- app/src/components/Assets/Reporting/OOW.js
+++ app/src/components/Assets/Reporting/OOW.js
@@ -1,51 +1,64 @@
 import React, { Component } from 'react';
-import { Panel, Table } from 'react-bootstrap';
+import ReactTable from "react-table";
 import moment from 'moment';
 
 export default class OOWDevices extends Component{
 
     render(){
         const data = this.props.data.filter(item => 
-            item.warranty?moment(item.inDate).isBefore(moment().subtract(parseInt(item.warranty.replace(/\D+/, '')), 'years')):true)
+            item.warranty?moment(item.inDate).isBefore(moment().subtract(parseInt(item.warranty.replace(/\D+/, '')), 'years')):true);
+
+        const columns = [
+            {
+                Header: "Serial Number",
+                accessor: "serial_number",
+                style: { 'white-space': 'unset' }
+            },
+            {
+                Header: "Model",
+                accessor: "model",
+                style: { 'white-space': 'unset' }
+            },
+            {
+                Header: "Contract",
+                accessor: "contract"
+            },
+            {
+                Header: "Owner",
+                accessor: "owner"
+            },
+            {
+                Header: "Warranty",
+                accessor: "warranty"
+            },
+            {
+                Header: "Warranty Provider",
+                accessor: "warranty_provider"
+            },
+            {
+                Header: "In Date",
+                accessor: "inDate",
+                Cell: val => moment(val.value).format('YYYY-MM-DD')
+            },
+            {
+                Header: "Broken?",
+                accessor: "broken",
+                Cell: val => val.value===0?'N':'Y'
+            },
+            {
+                Header: "Comment",
+                accessor: "comment",
+                style: { 'white-space': 'unset' }
+            }
+        ];
 
         return(
-            <Panel bsStyle='danger'>
-                <Panel.Heading>
-                    <Panel.Title componentClass='h3'>Out of Warranty Devices</Panel.Title>
-                </Panel.Heading>
-                <Panel.Body>
-                    <Table striped>
-                        <thead>
-                            <tr>
-                                <th>Serial Number</th>
-                                <th>Model</th>
-                                <th>Contract</th>
-                                <th>Owner</th>
-                                <th>Warranty</th>
-                                <th>Warranty Provider</th>
-                                <th>In Date</th>
-                                <th>Broken</th>
-                                <th>Comment</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map(item =>
-                                <tr>
-                                    <td>{item.serial_number}</td>
-                                    <td>{item.model}</td>
-                                    <td>{item.contract}</td>
-                                    <td>{item.owner}</td>         
-                                    <td>{item.warranty}</td>
-                                    <td>{item.warranty_provider}</td>
-                                    <td>{moment(item.inDate).utc().format('YYYY-MM-DD')}</td>
-                                    <td>{item.broken?'Y':'N'}</td>
-                                    <td>{item.comment}</td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </Table>
-                </Panel.Body>
-            </Panel>
-        )
+            <React.Fragment>
+                <ReactTable
+                    data={data}
+                    columns={columns}
+                />
+            </React.Fragment>
+        );
     }
 }
