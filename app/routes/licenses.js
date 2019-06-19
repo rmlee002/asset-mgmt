7a553e1aeea86c4b4d47a57698e16f9a227bfe32
Ryan Lee <ryan.lee@vidoori.com>
2019-06-19 11:44:32 -0400

    Removed ability to assign retired software to user

--- app/routes/licenses.js
+++ app/routes/licenses.js
@@ -18,13 +18,13 @@ router.post('/', (req,res)=>{
         ON licenses.software_id = software.software_id\
         WHERE licenses.software_id=? AND licenses.end IS NULL', req.body.software_id, (err,results)=>{
         if(err){
-            console.log(err)
+            console.log(err);
             res.status(500).send("Database query error")
         }
         else{
             connection.query('SELECT software.name FROM software WHERE software_id=?', req.body.software_id, (err,results2)=>{
                 if (err){
-                    console.log(err)
+                    console.log(err);
                     res.status(500).send("Database query error")
                 }
                 res.send({
@@ -34,63 +34,63 @@ router.post('/', (req,res)=>{
             })
         }
     })
-})
+});
 
 router.post('/add', (req,res)=>{
     connection.query('INSERT INTO licenses (emp_id, software_id, start) VALUES (?,?,?)', 
     [req.body.emp_id, req.body.software_id,req.body.start], (err,results)=>{
         if (err){
-            console.log(err)
+            console.log(err);
             res.status(500).send("Database query error")
         }
         else{
             res.status(200).send('Success')
         }
     })
-})
+});
 
 router.post('/retire', (req,res) => {
     connection.query('UPDATE licenses SET end=? WHERE software_id=? AND emp_id=? AND end IS NULL', [req.body.end, req.body.software_id, req.body.emp_id], (err,results)=>{
         if (err){
-            console.log(err)
+            console.log(err);
             res.status(500).send("Database query error")
         }
         res.status(200).send('Success')
     })
-})
+});
 
 router.post('/getUserData', (req,res)=>{
     connection.query('SELECT software.software_id, software.name, licenses.start, licenses.end FROM licenses JOIN software\
         ON licenses.software_id = software.software_id WHERE emp_id=?', req.body.emp_id, (err,results)=>{
             if (err){
-                console.log(err)
+                console.log(err);
                 res.status(500).send("Database query error")
             }
             res.send(JSON.stringify(results))
         })
-})
+});
 
 router.post('/getEmployees', (req,res)=>{
     connection.query('SELECT emp_id, first_name, last_name, email, affiliation, department\
         FROM employees WHERE archived=FALSE AND emp_id NOT IN\
         (SELECT emp_id FROM licenses WHERE software_id=? AND end IS NULL)', req.body.software_id, (err,results)=>{
             if (err){
-                console.log(err)
+                console.log(err);
                 res.status(500).send("Database query error")
             }
             res.send(JSON.stringify(results))
         })
-})
+});
 
 router.post('/getSoftware', (req,res)=>{
     connection.query('SELECT software_id, name FROM software WHERE software_id NOT IN \
-        (SELECT software_id FROM licenses WHERE emp_id=? AND end IS NULL)', req.body.emp_id, (err, results) => {
+        (SELECT software_id FROM licenses WHERE emp_id=? AND end IS NULL) AND archived IS FALSE', req.body.emp_id, (err, results) => {
             if (err) {
-                console.log(err)
+                console.log(err);
                 res.status(500).send("Database query error")
             }
             res.send(JSON.stringify(results))
         })
-})
+});
 
 module.exports = router;
