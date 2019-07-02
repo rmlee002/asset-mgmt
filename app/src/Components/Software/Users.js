47e77bfbb98b91251f61fd91cfbe90e973861b17
Ryan Lee <ryan.lee@vidoori.com>
2019-07-02 15:02:16 -0400

    Filter fix

--- app/src/Components/Software/Users.js
+++ app/src/Components/Software/Users.js
@@ -79,6 +79,8 @@ export default class Users extends Component{
     );
         
     handleFilter(options){
+        console.log(moment(item.start).utc());
+        console.log(options.start);
         this.setState({
             filtered: this.state.users.filter(item =>
                 moment(item.start).utc().isSameOrAfter(options.start)
