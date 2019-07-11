0f8579c222d7a285405bdfb466ff3654f88759d2
Ryan Lee <ryan.lee@vidoori.com>
2019-07-11 12:17:48 -0400

    Bug fix

--- app/server.js
+++ app/server.js
@@ -39,6 +39,10 @@ app.get('/checkToken', authorized, (req, res) => {
 
 app.use(express.static(path.join(__dirname, './build')));
 
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
